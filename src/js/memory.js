c3eabf6b59907ae0f0c3e60b4d2d4aa13572028c
PavelKonstantinovich <pashakonstantinovich@gmail.com>
2021-12-07 01:10:52 +0300

    toDo

---
+++ src/js/memory.js
@@ -0,0 +1,34 @@
+class ToDoMemory {
+  data = JSON.parse(localStorage.getItem('data')) || []
+  constructor() {
+
+    this.#init()
+  }
+
+  #init() {
+    this.handleBeforeUnload = this.#handleBeforeUnload.bind(this)
+    this.handleDOMReady = this.#handleDOMReady.bind(this)
+
+    window.addEventListener('beforeunload', this.handleBeforeUnload)
+    window.addEventListener('DOMContentLoaded', this.handleDOMReady)
+  }
+
+  #handleBeforeUnload() {
+    const json = JSON.stringify(this.data)
+    localStorage.setItem('data', json)
+  }
+
+  #handleDOMReady(event) {
+    event.preventDefault()
+    const dataFromStorage = localStorage.getItem('data')
+
+    if (dataFromStorage) {
+      this.data = JSON.parse(dataFromStorage)
+
+      const eventRenderNeed = new Event('render:need')
+      window.dispatchEvent(eventRenderNeed)
+    }
+  }
+}
+
+export { ToDoMemory }
